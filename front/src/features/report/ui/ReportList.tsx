3d8481637d2edd6ccb0e12c911bcb06206b350ac
Tatsuya <49563669+kmno4hno3@users.noreply.github.com>
2025-03-01 13:14:27 +0900

    日報一覧の日報選択で詳細表示

--- front/src/features/report/ui/ReportList.tsx
+++ front/src/features/report/ui/ReportList.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Month, Report } from "@/src/entities/files/type";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { invoke } from "@tauri-apps/api/core";
 
 interface props {
   selectedMonthReports?: Month;
@@ -13,6 +16,27 @@ export const ReportList = ({
   selectedYear,
   selectedMonth,
 }: props) => {
+  // TODO:ファイルをここで読み込む
+  const [files, setFiles] = useState<string>();
+  const [error, setError] = useState("");
+  const fetchFile = async (path: string) => {
+    try {
+      1;
+      setError("");
+      const result = await invoke<string>("list_directory", {
+        path,
+      });
+      setFiles(result);
+    } catch (err) {
+      setError("ファイルの読み込みに失敗しました");
+      console.error("Error", err);
+    }
+  };
+
+  useEffect(() => {
+    // fetchFile(filePath);
+  }, []);
+
   return (
     selectedMonthReports &&
     selectedYear &&
@@ -23,13 +47,18 @@ export const ReportList = ({
         </h2>
         <div className="grid gap-4">
           {selectedMonthReports.reports.map((report: Report) => (
-            <button
-              key={report.date}
-              className="text-left p-4 border rounded-lg hover:bg-gray-100 transition-colors"
+            <Link
+              href={`/report/list/${selectedYear}/${selectedMonth}/${report.date}`}
+              key={`${report.date}`}
             >
-              <h3 className="font-semibold">{report.date}</h3>
-              <p className="text-gray-600 truncate">{report.date}</p>
-            </button>
+              <button
+                key={report.date}
+                className="text-left p-4 border rounded-lg hover:bg-gray-100 transition-colors"
+              >
+                <h3 className="font-semibold">{report.date}</h3>
+                <p className="text-gray-600 truncate">{report.date}</p>
+              </button>
+            </Link>
           ))}
         </div>
       </div>
--- front/src/features/report/ui/ReportWrapper.tsx
+++ front/src/features/report/ui/ReportWrapper.tsx
@@ -4,9 +4,7 @@ import type React from "react";
 import { usePathname } from "next/navigation";
 import { fileListAtom } from "@/src/entities/files/model";
 import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
-import { Report } from "@/src/entities/files/type";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState, useMemo } from "react";
 import { ReportLayout } from "./ReportLayout";
 import { ReportDetail } from "./ReportDetail";
 import { ReportList } from "./ReportList";
@@ -34,35 +32,21 @@ export const ReportWrapper = () => {
     }
   }, [pathname]);
 
-  const [files, setFiles] = useState<string>();
-  const [error, setError] = useState("");
-  const fetchFile = async (path: string) => {
-    try {
-      1;
-      setError("");
-      const result = await invoke<string>("list_directory", {
-        path,
-      });
-      setFiles(result);
-    } catch (err) {
-      setError("ファイルの読み込みに失敗しました");
-      console.error("Error", err);
-    }
-  };
-
-  useEffect(() => {
-    // fetchFile(filePath);
-  }, []);
-
-  const selectedYearReports = reportData.find((yearData) => {
-    return yearData.year === Number(selectedYear);
-  });
-  const selectedMonthReports = selectedYearReports?.months.find((monthData) => {
-    return monthData.month === Number(selectedMonth);
-  });
-  const selectedDateReport = selectedMonthReports?.reports.find((report) => {
-    return report.date === Number(selectedDate);
-  });
+  const selectedYearReports = useMemo(() => {
+    return reportData.find((yearData) => {
+      return yearData.year === Number(selectedYear);
+    });
+  }, [selectedYear]);
+  const selectedMonthReports = useMemo(() => {
+    return selectedYearReports?.months.find((monthData) => {
+      return monthData.month === Number(selectedMonth);
+    });
+  }, [selectedMonth]);
+  const selectedDateReport = useMemo(() => {
+    return selectedMonthReports?.reports.find((report) => {
+      return report.date === Number(selectedDate);
+    });
+  }, [selectedDate]);
 
   const renderChildren = () => {
     if (selectedDateReport) {
